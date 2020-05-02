4989147d19d112357eebf054ab4dbff9a7da67a4
david mudd <davidmudd@outlook.com>
2020-05-02 15:02:37 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+msg = 'Hello World'
+
+print(msg)
+# ----------- Math Operators ------------
+
+print(2 ** 3)
+
+print(22 % 8)
+
+print(22 // 8)
+
+print(22 / 8)
+
+print(3 * 5)
+
+print(5 - 2)
+
+print(2 + 2)
+
+
+
